7721abeb95291ddad070062dcfff8b3c990b5928
Mohamed Abirou <abiroumohamed58@gmail.com>
2024-02-15 19:21:22 +0100

    feat & style: orders entity

---
+++ app/(dashboard)/[storeId]/(routes)/orders/_components/order-client.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { Heading } from "@/components/heading";
+import { Separator } from "@/components/ui/separator";
+import { OrderColumn, columns } from "./columns";
+import { DataTable } from "@/components/data-table";
+
+interface OrderClientProps {
+  data: OrderColumn[];
+}
+
+export const OrderClient = ({ data }: OrderClientProps) => {
+  return (
+    <>
+      <Heading
+        title={`Orders (${data.length})`}
+        description="Manage orders for your store"
+      />
+      <Separator />
+      <DataTable
+        columns={columns}
+        data={data}
+        searchKey="products"
+        placeholder="orders"
+      />
+    </>
+  );
+};
